f8ade9e0acbd017c9b7452a211f1da07ec1ceae2
mondo998 <41578169+mondo998@users.noreply.github.com>
2019-10-27 20:18:51 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,12 +2,12 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '120',
+  ver: '121',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
     enable: true,
-    ver: 62
+    ver: 74
   },
 
   // 节点配置
@@ -16,8 +16,9 @@ jsproxy_config({
       label: '演示服务-香港节点',
       lines: {
         // 主机:权重
+        'node-aliyun-hk-0.etherdream.com:8443': 1,
         'node-aliyun-hk-1.etherdream.com:8443': 1,
-        'node-aliyun-hk-2.etherdream.com:8443': 2,
+        'node-aliyun-hk-2.etherdream.com:8443': 1,
       }
     },
     'demo-sg': {
@@ -27,18 +28,21 @@ jsproxy_config({
       },
     },
     'demo-cf': {
-      label: 'cfworker',
+      label: 'CF',
       lines: {
-          'jsproxy.sysuyaowan.workers.dev': 1, 
+        'jsproxy.sysuyaowan.workers.dev': 1,
       }
     },
     // 该节点用于加载大体积的静态资源
     'cfworker': {
       label: '',
       hidden: true,
-     lines: {
-        'jsproxy.sysuyaowan.workers.dev': 1,
+      lines: {
+        // 收费版（高权重）
         'node-cfworker-2.etherdream.com': 4,
+
+        // 免费版（低权重，分摊一些成本）
+        // 每个账号每天 10 万次免费请求，但有频率限制
         'b.007.workers.dev': 1,
         'b.hehe.workers.dev': 1,
         'b.lulu.workers.dev': 1,
@@ -50,8 +54,8 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'mysite',
-  // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
+  // node_default: 'mysite',
+  node_default: /github\.io$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
    * 加速节点
@@ -62,10 +66,10 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  // assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
-  assets_cdn: 'assets/',
+  // assets_cdn: 'assets/',
 
   // 首页路径
   index_path: 'index_v3.html',
