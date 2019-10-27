65583976e4963182d9c0275005d1e3e8b81f9a12
mondo998 <41578169+mondo998@users.noreply.github.com>
2019-10-27 20:10:51 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '118',
+  ver: '119',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -26,7 +26,7 @@ jsproxy_config({
         'node-aliyun-sg.etherdream.com:8443': 1,
       },
     },
-    'mysite': {
+    'demo-cf': {
       label: 'cfworker',
       lines: {
           'jsproxy.sysuyaowan.workers.dev': 1, 
