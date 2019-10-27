4732cedfb22e3b5a5af26bfe2239613ffc743b5e
mondo998 <41578169+mondo998@users.noreply.github.com>
2019-10-27 20:09:06 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '117',
+  ver: '118',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -27,9 +27,9 @@ jsproxy_config({
       },
     },
     'mysite': {
-      label: '当前站点',
+      label: 'cfworker',
       lines: {
-          [location.host]: 1, 
+          'jsproxy.sysuyaowan.workers.dev': 1, 
       }
     },
     // 该节点用于加载大体积的静态资源
@@ -38,6 +38,11 @@ jsproxy_config({
       hidden: true,
      lines: {
         'jsproxy.sysuyaowan.workers.dev': 1,
+        'node-cfworker-2.etherdream.com': 4,
+        'b.007.workers.dev': 1,
+        'b.hehe.workers.dev': 1,
+        'b.lulu.workers.dev': 1,
+        'b.jsproxy.workers.dev': 1,
       }
     }
   },
